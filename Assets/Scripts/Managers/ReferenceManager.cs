26bca8638dff5da5d30bb22a15a77455e708bdac
CTN <info@ctn-originals.com>
2025-11-07 22:29:36 +0100

    Basic scripts setup

---
+++ Assets/Scripts/Managers/ReferenceManager.cs
@@ -0,0 +1,8 @@
+using CTNOriginals.PlatformReplayer.Utilities;
+using UnityEngine;
+
+namespace CTNOriginals.PlatformReplayer.Managers {
+	public class ReferenceManager : Singleton<ReferenceManager> {
+		
+	}
+}
---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+namespace CTNOriginals.PlatformReplayer.Player {
+	public class PlayerController : MonoBehaviour {
+		
+	}
+}
