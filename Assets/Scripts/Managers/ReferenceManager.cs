8db943bc037a11be7e76a2d550df5e7bf1d33ecd
CTN <info@ctn-originals.com>
2025-11-08 20:14:18 +0100

    ReferenceManager: Added essential fields

--- Assets/Scripts/Managers/ReferenceManager.cs
+++ Assets/Scripts/Managers/ReferenceManager.cs
@@ -5,5 +5,13 @@ using UnityEngine;
 namespace CTNOriginals.PlatformReplayer.Managers {
 	public class ReferenceManager : Singleton<ReferenceManager> {
 		public PlayerController PlayerController;
+
+		public Transform ReplayerHolder;
+		public GameObject PlayerRecordingPrefab;
+		public Vector2 PlayerStartPosition;
+
+		private void Start() {
+			this.PlayerStartPosition = this.PlayerController.transform.position;
+		}
 	}
 }
